d61ea140eab6894bb40c9dbbed3c8e0acb691826
azaria amanda <azaryaamanda@gmail.com>
2024-09-06 09:19:43 +0700

    ini commit pertama

---
+++ Latihan02.java
@@ -0,0 +1,32 @@
+/**
+ * Latihan02
+ */
+public class Latihan02 {
+
+    public static void main(String[] args) {
+        /**System.out.println("-------------------------------------------------------");
+        System.out.println("--Data Mahasiswa Jurusan Teknologi Informasi POLINEMA--");
+        System.out.println("-------------------------------------------------------");
+        System.out.println("Nama      : Azaria Amanda");
+        System.out.println("Nim       : 244107060060");
+        System.out.println("Kelas     : SIB 1A");
+        System.out.println("No Absen  : 02");
+        System.out.println("Prodi     : Sistem Informasi Bisnis");
+        System.out.println("Alamat    : Sumbermanjing Wetan,Malang");
+        System.out.println("No HP     : 085732925295");
+        System.out.println("Hobi      : Bermain Bola Volly");*/
+
+
+
+
+
+
+
+
+
+        System.out.print("Nama saya Azaria Amanda");
+        //System.out.print("\nsaya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+
+    }
+}
+
